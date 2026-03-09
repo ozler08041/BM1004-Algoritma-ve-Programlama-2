d7cf5e676eab8a672760eb00e89fabbcc8b05a1f
Havva Gülsüm Özler <havvagulsumozler@gmail.com>
2026-03-09 14:49:36 +0300

    Add files via upload

---
+++ Hafta-1/Soru28/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Soru28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan bir sayı alıp, 1'den bu sayıya kadar olan tüm sayıları ekrana yazdıran bir program yazınız.
+            Console.Write("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= sayi; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
